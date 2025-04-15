8f016bd659ecc1b82b05938703e89048c2fd9d94
MahekAlwani <mehakalwani.bscskdkf22@iba-suk.edu.pkq>
2025-04-15 12:25:19 -0700

    title changed

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pickle
 
 st.title("Salary Predictor")
-
+st.button("Predict salary")
 with open("model.pkl") as file:
     model = pickle.load(file)
 
     user_input = st.number_input("Experience years", min_value=0.0, max_value=50.0, value=0.0, step=0.1)
-
+if st.button:
     st.success(model.predict([[user_input]]))
